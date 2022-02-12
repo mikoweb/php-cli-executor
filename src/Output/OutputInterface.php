2d6e2d49db9fb8627cb14dc9c99b184bb27f883f
Rafał Mikołajun <root@rmweb.pl>
2022-02-12 19:49:42 +0100

    add output interfaces

---
+++ src/Output/OutputInterface.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Copyright (c) Rafał Mikołajun 2022.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mikoweb\CLIExecutor\Output;
+
+interface OutputInterface
+{
+}
---
+++ src/Output/OutputParserInterface.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Copyright (c) Rafał Mikołajun 2022.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mikoweb\CLIExecutor\Output;
+
+interface OutputParserInterface
+{
+    public function parse(string $output): array;
+}
